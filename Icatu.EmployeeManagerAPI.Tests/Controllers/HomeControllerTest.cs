6f18741e156f302873334d0ff974fd9993e3faeb
luis.siqueira <luis.siqueira@globalhitss.com.br>
2018-09-18 13:33:54 -0300

    Adicionar arquivos de projeto.

---
+++ Icatu.EmployeeManagerAPI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Icatu.EmployeeManagerAPI;
+using Icatu.EmployeeManagerAPI.Controllers;
+
+namespace Icatu.EmployeeManagerAPI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Organizar
+            HomeController controller = new HomeController();
+
+            //var result3 = fc.Get();
+            
+
+            // Agir
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
